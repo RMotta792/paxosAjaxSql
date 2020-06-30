076ae3fc4a71952182f3eb39c65565c45274609e
Ryan Motta <mrryanmotta@gmail.com>
2020-06-30 13:52:00 -0500

    initial commit and set up

---
+++ server/modeules/birds.route.js
@@ -0,0 +1,9 @@
+// requires
+const express = require( 'express' );
+const router = express.Router();
+// routes
+router.get('/', (req, res)=>{
+    console.log( '/birds GET');
+    
+    
+})
---
+++ server/modeules/pool.js
@@ -0,0 +1,14 @@
+//requires
+const pg = require( 'pg' );
+// globals
+const pool = new pg.Pool({
+    database: 'sql_test',
+    host: 'localhost',
+    port: 5432,
+    max: 15,
+    idleTimeoutMillis: 30000
+})
+// export
+modeule.exports = pool;
+
+
