85e7762891203e2e7a9bb1e005a41b5039e06f0b
Ryan Motta <mrryanmotta@gmail.com>
2020-07-01 14:48:17 -0500

    Inital commit. File directory and basic set up completed.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-const express =
+// Requires
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+
+// Uses
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( express.static( 'server/public' ) );
+
+// Globals
+const port = 5000;
+
+// Server up
+app.listen( port, () => {
+    console.log('server up!', port );
+})
